60fe01c75d68c03d1d9a1cdc4e70ef578c10adb2
Ricardo Lopez <125326500+RicardoLDev@users.noreply.github.com>
2023-02-22 12:57:17 -0600

    finishing up

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+const computerRock = document.getElementById("comp-rock");
+const computerPaper = document.getElementById("comp-paper");
+const computerScissors = document.getElementById("comp-scissors");
+const userRock = document.getElementById("Rock");
+const userPaper = document.getElementById("Paper");
+const userScissors = document.getElementById("Scissors");
 let userChoice
 let computerChoice
 let result 
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  console.log(e)
   userChoice = e.target.id
-  console.log(userChoice)
-  userChoiceDisplay.innerHTML = userChoice
-  generateComputerChoice()
-  getResult()
+  if (userChoice.length > 0){
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+    removeChoices()
+  }
 }))
 
 
@@ -36,23 +43,50 @@ function getResult(){
         result = "It's a Draw"
     }
     if (computerChoice === "Rock" && userChoice === "Scissors"){
+        computerScissors.remove();
+        computerPaper.remove();
         result = "Sorry, you lost"
     }
     if (computerChoice === "Scissors" && userChoice === "Paper"){
+        computerRock.remove();
+        computerPaper.remove();
         result = "Sorry, you lost"
     }
     if (computerChoice === "Paper" && userChoice === "Rock"){
+        computerRock.remove();
+        computerScissors.remove();
         result = "Sorry, you lost"
     }
     if (computerChoice === "Paper" && userChoice === "Scissors"){
+        computerRock.remove();
+        computerScissors.remove();
         result = "Congrats you won!"
     }
     if (computerChoice === "Rock" && userChoice === "Paper"){
+        computerScissors.remove();
+        computerPaper.remove();
         result = "Congrats you won!"
     }
     if (computerChoice === "Scissors" && userChoice === "Rock"){
+        computerRock.remove();
+        computerPaper.remove();
         result = "Congrats you won!"
     }
 
     resultDisplay.innerHTML = result
+}
+
+function removeChoices(){
+    if (userChoice === "Rock"){
+        userPaper.remove();
+        userScissors.remove();
+    }
+    if (userChoice === "Paper"){
+        userRock.remove();
+        userScissors.remove();
+    }
+    if (userChoice === "Scissors"){
+        userPaper.remove();
+        userRock.remove();
+    }
 }
